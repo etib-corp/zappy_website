194d36a0bdf86dddd2fd5c8f1b1d78b5843a66e7
Neisan <nathan.maillot@epitech.eu>
2024-06-14 17:22:22 +0400

    feat: add flowbite

--- zappy/src/App.js
+++ zappy/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
-import './output.css';
+
+import { Button, ButtonGroup, Card } from 'flowbite-react';
+
+import './App.css';
 
 function App() {
   return (
@@ -9,14 +12,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <ButtonGroup>
+          <Button>Learn React</Button>
+          <Button>Learn Next</Button>
+          <Button>Learn Vue</Button>
+          <Button>Learn WP</Button>
+        </ButtonGroup> */}
+        <Card>Yes</Card>
       </header>
     </div>
   );
