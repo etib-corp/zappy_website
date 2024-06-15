b71a319c2c045098986383d971fbaf40de3575bc
LAURET Benjamin <benjamin.lauret@epitech.eu>
2024-06-15 20:13:41 +0400

    Fix: Home section alignement

--- zappy/src/components/Footer.tsx
+++ zappy/src/components/Footer.tsx
@@ -2,12 +2,14 @@ import { Footer, DarkThemeToggle } from "flowbite-react";
 
 export default function Footer_Component () {
     return (
-        <Footer container className="">
+        <Footer container className="min-w-screen-md flex items-center space-x-14">
         <Footer.Copyright href="#" by="by Etib Corp™" year={2024} />
         <Footer.LinkGroup>
-            <Footer.Link href="#">GitHub</Footer.Link>
-            <Footer.Link href="#">Docs</Footer.Link>
-            <DarkThemeToggle />
+            <div className="flex items-center space-x-4">
+                <Footer.Link href="#">GitHub</Footer.Link>
+                <Footer.Link href="#">Docs</Footer.Link>
+                <DarkThemeToggle />
+            </div>
         </Footer.LinkGroup>
         </Footer>
     );
--- zappy/src/components/Navbar.tsx
+++ zappy/src/components/Navbar.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 
-import { DarkThemeToggle } from "flowbite-react";
+import { DarkThemeToggle, SidebarLogo } from "flowbite-react";
 
 import { Navbar } from "flowbite-react";
 
 export function Narbar_Component() {
   return (
-    <div className="py-6 px-8">
-      <Navbar className="py-10" fluid rounded>
-        <Navbar.Brand className="px-3 flex-col" href="https://etib.tech">
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Zappy</span>
-        </Navbar.Brand>
-        <div className="flex justify-center items-center">
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
+  <div className="py-6 px-8">
+    <Navbar className="py-10" fluid rounded>
+      <Navbar.Brand className="px-3" href="https://etib.tech">
+        <img src="zappy.png" alt="Zappy" className="h-20"/>
+      </Navbar.Brand>
+      <div className="flex justify-center">
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <div className="flex items-center space-x-10">
           <Navbar.Link href="#" active>
             Home
           </Navbar.Link>
           <Navbar.Link href="#">GitHub</Navbar.Link>
           <Navbar.Link href="#">Docs</Navbar.Link>
-          <DarkThemeToggle />
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+        </div>
+        <DarkThemeToggle />
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
   );
 }
