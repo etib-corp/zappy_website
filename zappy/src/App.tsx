f46b530d1d6b6137b26d259634b27e0517a9a4b9
Neisan <nathan.maillot@epitech.eu>
2024-06-15 18:20:40 +0400

    feat: add onyx page

--- zappy/src/App.tsx
+++ zappy/src/App.tsx
@@ -1,28 +1,17 @@
-import { Article } from "./components/Bloc";
-import { Narbar_Component } from "./components/Navbar";
+import Home from "./pages/Home";
+import Onyx from "./pages/Onyx";
 
-import Footer_Component from "./components/Footer";
-import Quote, { Title } from "./components/Quote";
-import Presentation, { Team } from "./components/Presentation";
-
-import home from "./JSON/home-description.json";
-import GetButton from "./components/GetButton";
-import Roadmap from "./components/Roadmap";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <main >
-        <div className="dark:bg-gray-800 py-2">
-          <Narbar_Component />
-          <Presentation />
-          <Quote picture="https://companieslogo.com/img/orig/epitech-eu-28fcad28.png?t=1701708648" name="Epitech" description="IT School" text="Courage, Excellence & Solidarity." />
-          <Article properties={home} />
-          <Title text="Our Team:" />
-          <Team />
-          <Roadmap />
-          <GetButton />
-          <Footer_Component />
-        </div>
+    <main>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/onyx" element={<Onyx />} />
+        </Routes>
+      </div>
     </main>
   );
 }
--- zappy/src/index.tsx
+++ zappy/src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
+  document.getElementById('root')
 );
