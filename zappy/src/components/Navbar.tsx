92d154936418400cf6ea8150ecde800da35f44a4
Neisan <nathan.maillot@epitech.eu>
2024-06-17 18:16:05 +0400

    feat: add doc into link

--- zappy/src/components/Navbar.tsx
+++ zappy/src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export function Narbar_Component() {
             Home
           </Navbar.Link>
           <Navbar.Link href="https://github.com/MAILLOTNathan/zappy">GitHub</Navbar.Link>
-          <Navbar.Link href="#">Docs</Navbar.Link>
+          <Navbar.Link href="doc/html/index.html">Docs</Navbar.Link>
           <DarkThemeToggle />
         </div>
       </Navbar.Collapse>
