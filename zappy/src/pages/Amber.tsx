9b8e55d806d2d50d134f19aec7c6135595796cad
LAURET Benjamin <benjamin.lauret@epitech.eu>
2024-06-17 18:18:48 +0400

    Delete: Button

--- zappy/src/pages/Amber.tsx
+++ zappy/src/pages/Amber.tsx
@@ -1,5 +1,5 @@
-import Footer_Component from "../components/Footer";
-import { Narbar_Component } from "../components/Navbar";
+import FooterComponent from "../components/Footer";
+import { NavbarComponent } from "../components/Navbar";
 import { Article } from "../components/Bloc";
 import { Title } from "../components/Quote";
 
@@ -9,7 +9,7 @@ import RFC from "../components/RFC";
 export default function Amber() {
     return (
         <div className="dark:bg-gray-800 py-2 overflow-x-hidden">
-            <Narbar_Component />
+            <NavbarComponent />
             <div className="max-md:mx-8 flex items-center justify-center rounded my-10 bg-gradient-to-r from-purple-500 to-pink-500">
                 <h1 className="max-lg:ml-20 max-sm:ml-5 max-sm:text-md max-md:text-2xl max-md:ml-10 text-4xl font-semibold font-sans italic overline">
                     Amber
@@ -39,7 +39,7 @@ export default function Amber() {
             </div>
             <Title text="Let's take a look to the RFC" />
             <RFC />
-            <Footer_Component />
+            <FooterComponent />
         </div>
     )
 }
--- zappy/src/pages/Turing.tsx
+++ zappy/src/pages/Turing.tsx
@@ -1,11 +1,11 @@
-import Footer_Component from "../components/Footer";
-import { Narbar_Component } from "../components/Navbar";
+import FooterComponent from "../components/Footer";
+import { NavbarComponent } from "../components/Navbar";
 import { Article } from "../components/Bloc";
 
 export default function Turing () {
     return (
         <div className="dark:bg-gray-800 py-2 overflow-x-hidden">
-            <Narbar_Component />
+            <NavbarComponent />
             <div className="max-md:mx-8 flex items-center justify-center my-10 bg-gradient-to-r from-pink-500 to-red-500">
                 <h1 className="max-md:text-2xl max-md:ml-10 text-4xl">
                     Turing
@@ -14,7 +14,7 @@ export default function Turing () {
                     <Article properties={{ title: "Dive into immersion", content: "Experience the simulation" }} />
                 </div>
             </div>
-            <Footer_Component />
+            <FooterComponent />
         </div>
     );
 }
